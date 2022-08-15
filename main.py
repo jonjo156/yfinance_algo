12f58fef48096aa3709fde7e542578353d758cff
jonjo156 <jonjowadwa@live.co.uk>
2022-08-15 18:45:29 +0100

    updated main.py for new module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import yfinance as yf
 from dividend_yield import get_dividend_yield
 from financials_check import financials_check
+from gross_profit_margin import gross_profit_margin_check
 
 ticker = "GRG.L"
 
 print(get_dividend_yield(ticker))
 print(financials_check(ticker))
+print(gross_profit_margin_check(ticker))
